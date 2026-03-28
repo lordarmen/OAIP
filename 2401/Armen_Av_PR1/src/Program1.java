9b3924ae90a85dc84900a5edcf0d0d4cb2c057cd
Armen <107412632+lordarmen@users.noreply.github.com>
2026-03-28 17:19:00 +0300

    LR_6
    Александру Вячеславовичу

---
+++ 2401/Armen_Av_PR1/src/Program1.java
@@ -0,0 +1,15 @@
+public class Program1 {
+    public static void main(String[] args) {
+
+        double a = 5.0;
+        double b = 3.0;
+
+        double S = a * b;
+        double P = 2 * (a + b);
+
+        System.out.println("Сторона a = " + a);
+        System.out.println("Сторона b = " + b);
+        System.out.println("Площадь S = " + S);
+        System.out.println("Периметр P = " + P);
+    }
+}
