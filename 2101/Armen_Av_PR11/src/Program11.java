261234fe3712d3d8901209feb3e2aa2c1aa15d30
Armen <107412632+lordarmen@users.noreply.github.com>
2026-03-28 17:17:43 +0300

    LR_3
    Александру Вячеславовичу

---
+++ 2101/Armen_Av_PR11/src/Program11.java
@@ -0,0 +1,11 @@
+public class Program11 {
+    public static void main(String[] args) {
+
+        int year = 1905;
+
+        int century = (year - 1) / 100 + 1;
+
+        System.out.println("Год = " + year);
+        System.out.println("Столетие = " + century);
+    }
+}
